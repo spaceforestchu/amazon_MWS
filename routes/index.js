dd7c6a37bf3ff5e1329896f03de74d018a0c825d
Kenton <kentonlin1@gmail.com>
2016-11-28 16:01:29 -0500

    working mws listorder call

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,57 @@
 var express = require('express');
 var router = express.Router();
 var result;
-var mws = require('amazon-mws-node')({
-    AmzSecretKey: 'nySEuqkG2coIW53NER',
-    AWSAccessKeyId: 'AKIAJOVYKDEC5IYAQR3A'
-  });
+// var mws = require('amazon-mws-node')({
+//     AmzSecretKey: 'nySEuqkG2coIW53NER',
+//     AWSAccessKeyId: 'AKIAJOVYKDEC5IYAQR3A'
+//   });
 
-  mws({
-    method: 'GET',
-    base: 'mws.amazonservices.com',
-    endpoint: '/Orders/2013-09-01',
-    params: {
-      'Action': 'ListOrders',
-      'CreatedAfter': '2016-11-01',
-      'MarketplaceId.Id.1': 'ATVPDKIKX0DER',
-      'SellerId': 'A2LX4IL6EQVA63',
-      'MWSAuthToken': 'amzn.mws.64abd052-d8bc-adc7-0d2e-35a0e51acb5f',
-      'Version': '2013-09-01'
-    },
-    callback: function (error, response, body) {
-      result = body;
-      console.log('this is the result'); 
-      console.log(body);
-    }
-  });
+//   mws({
+//     method: 'GET',
+//     base: 'mws.amazonservices.com',
+//     endpoint: '/Orders/2013-09-01',
+//     params: {
+//       'Action': 'ListOrders',
+//       'CreatedAfter': '2016-09-01',
+//       'MarketplaceId.Id.1': 'ATVPDKIKX0DER',
+//       'SellerId': 'A2LX4IL6EQVA63',
+//       'MWSAuthToken': 'amzn.mws.64abd052-d8bc-adc7-0d2e-35a0e51acb5f',
+//       'Version': '2013-09-01'
+//     },
+//     callback: function (error, response, body) {
+//       result = body;
+//       console.log('this is the result'); 
+//       console.log(body);
+//     }
+//   });
 
 // mws-sdk-promises
-// var result;
-// var MWS = require('mws-sdk-promises'),
-// client = new MWS.Client('AKIAJOVYKDEC5IYAQR3A', 'nySEuqkG2coIW53NER', 'A2LX4IL6EQVA63', {}),
-// MarketplaceId = "ATVPDKIKX0DER";
-//
-//
-// function getListOrders(client, args) {
-// var req = MWS.Orders.requests.ListOrders();
-// req.set(args);
-// return client.invoke(req);
-// }
-// var date = new Date();
-// getListOrders(client, {
-// MarketplaceId: MarketplaceId,
-// MaxResultsPerPage: 10,
-// CreatedAfter: new Date(2016, 1, 1),
-// CreatedBefore: new Date(2016, 11, 23)
-// })
-// .catch(function(error) {
-// console.error(error);
-// })
-// .then(function(RESULT){
-// console.log("--------");
-// console.log(JSON.stringify(RESULT));
-// result = JSON.stringify(RESULT);
-// console.log("--------");
-// });
+var result;
+var MWS = require('mws-sdk-promises'),
+client = new MWS.Client('AKIAJOVYKDEC5IYAQR3A', 'nySEuqkG2coIW53NERc9qbUU03edNFktQcqobkTR', 'A2LX4IL6EQVA63', {}),
+MarketplaceId = "ATVPDKIKX0DER";
+
+function getListOrders(client, args) {
+var req = MWS.Orders.requests.ListOrders();
+req.set(args);
+return client.invoke(req);
+}
+var date = new Date();
+getListOrders(client, {
+MarketplaceId: MarketplaceId,
+MaxResultsPerPage: 10,
+CreatedAfter: new Date(2015, 09, 01),
+CreatedBefore: new Date(2016, 10, 21)
+})
+.catch(function(error) {
+console.error(error);
+})
+.then(function(RESULT){
+console.log("--------");
+console.log(JSON.stringify(RESULT));
+result = JSON.stringify(RESULT);
+console.log("--------");
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
